a8ed81ccff5938956e408dd1167faf2df8169a1f
jeeva30082001 <hariharan10202@gmail.com>
2025-09-09 21:18:45 +0530

    docker

--- test.mjs
+++
@@ -1,8 +0,0 @@
-import { handler } from "./index.mjs";
-
-const event = {
-  httpMethod: "POST",
-  body: JSON.stringify({ id: "1", task: "Learn AWS" }),
-};
-
-handler(event).then((res) => console.log(res));
